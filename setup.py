f648ecd0f9ecaf7396243b82ef00665d37fc513f
Dyno (Hongjun) Fu <dyno.fu@gmail.com>
2011-03-14 21:39:41 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+setup(
+    name='doorkeeper',
+    version='0.1',
+    description='',
+    author='',
+    author_email='',
+    url='',
+    install_requires=[
+        "Pylons>=1.0",
+        "SQLAlchemy>=0.5",
+    ],
+    setup_requires=["PasteScript>=1.6.3"],
+    packages=find_packages(exclude=['ez_setup']),
+    include_package_data=True,
+    test_suite='nose.collector',
+    package_data={'doorkeeper': ['i18n/*/LC_MESSAGES/*.mo']},
+    #message_extractors={'doorkeeper': [
+    #        ('**.py', 'python', None),
+    #        ('templates/**.mako', 'mako', {'input_encoding': 'utf-8'}),
+    #        ('public/**', 'ignore', None)]},
+    zip_safe=False,
+    paster_plugins=['PasteScript', 'Pylons'],
+    entry_points="""
+    [paste.app_factory]
+    main = doorkeeper.config.middleware:make_app
+
+    [paste.app_install]
+    main = pylons.util:PylonsInstaller
+    """,
+)
